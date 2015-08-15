b0c6143701266e23bdbb24617d92ce937d69d471
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-08-15 14:29:49 +0900

    Use latest HEAD docopt & cleanup no need classes & funcs

--- auto_argcomplete/fancy_docopt.py
+++ auto_argcomplete/fancy_docopt.py
@@ -2,13 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import re
-from docopt import Pattern
-
-
-def parse_section(name, source):
-    pattern = re.compile('^([^\n]*' + name + '[^\n]*\n?(?:[ \t].*?(?:\n|$))*)',
-                         re.IGNORECASE | re.MULTILINE)
-    return [s.strip() for s in pattern.findall(source)]
+from docopt import parse_section, Option
 
 
 def parse_defaults(doc, section_name=None):
@@ -20,54 +14,19 @@ def parse_defaults(doc, section_name=None):
         _, _, s = s.partition(':')
         split = re.split('\n[ \t]*(-\S+?)', '\n' + s)[1:]
         split = [s1 + s2 for s1, s2 in zip(split[::2], split[1::2])]
-        options = [Option.parse(s) for s in split if s.startswith('-')]
+        options = [OptionWithDesc.parse(s) for s in split if s.startswith('-')]
         defaults += options
     return defaults
 
 
-# just copied from https://github.com/docopt/docopt.git
-class LeafPattern(Pattern):
-
-    """Leaf/terminal node of a pattern tree."""
-
-    def __init__(self, name, value=None):
-        self.name, self.value = name, value
-
-    def __repr__(self):
-        return '%s(%r, %r)' % (self.__class__.__name__, self.name, self.value)
-
-    def flat(self, *types):
-        return [self] if not types or type(self) in types else []
-
-    def match(self, left, collected=None):
-        collected = [] if collected is None else collected
-        pos, match = self.single_match(left)
-        if match is None:
-            return False, left, collected
-        left_ = left[:pos] + left[pos + 1:]
-        same_name = [a for a in collected if a.name == self.name]
-        if type(self.value) in (int, list):
-            if type(self.value) is int:
-                increment = 1
-            else:
-                increment = ([match.value] if type(match.value) is str
-                             else match.value)
-            if not same_name:
-                match.value = increment
-                return True, left_, collected + [match]
-            same_name[0].value += increment
-            return True, left_, collected
-        return True, left_, collected + [match]
-
-
-class Option(LeafPattern):
-
-    def __init__(self, short=None, long=None, argcount=0, value=False, description=None):
+class OptionWithDesc(Option):
+    def __init__(self, short=None, long=None,
+                 argcount=0, value=False, description=None):
         assert argcount in (0, 1)
         if description is None:
             description = ''
-        self.short, self.long, self.argcount = short, long, argcount
         self.description = description
+        self.short, self.long, self.argcount = short, long, argcount
         self.value = None if value is False and argcount else value
 
     @classmethod
@@ -87,16 +46,6 @@ class Option(LeafPattern):
             value = matched[0] if matched else None
         description = description.strip()
         return class_(short, long, argcount, value, description)
-
-    def single_match(self, left):
-        for n, pattern in enumerate(left):
-            if self.name == pattern.name:
-                return n, pattern
-        return None, None
-
-    @property
-    def name(self):
-        return self.long or self.short
 
     def __repr__(self):
         return 'Option(%r, %r, %r, %r, %r)' % (self.short, self.long,
